e72a78afdb2a8724feca33a4476080fc00eca6a4
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2024-01-12 18:04:59 -0500

    Update CaratulaComponent.tsx

--- src/caratula/CaratulaComponent.tsx
+++ src/caratula/CaratulaComponent.tsx
@@ -1,30 +1,35 @@
+import { Link } from "react-router-dom";
 import { RegistroComponent } from "../registro/Registro";
-import './Caratula.css'
+import "./Caratula.css";
 
-export function CaratulaComponent(){
-    return(
-        <section className="flex flex-row flex-wrap justify-evenly py-10 px-3 caratula-bg">
-            <div className="grid content-evenly text-gray-700">
-                <h1 className="text-6xl font-extrabold">Educa Online</h1>
-                <h2 className="text-3xl md:text-4xl font-bold">Aprende con nosotros</h2>
-                <div>
-                    <p className="text-2xl ">
-                        Explora nuestros&nbsp;
-                        <a href="/" className="underline text-indigo-700">cursos</a>
-                    </p>
-                    <p className="text-2xl ">
-                        Y accede a ellos en nuestra&nbsp;
-                        <a href="/" className="underline text-indigo-700">
-                            aula&nbsp;
-                            <span><i className="fas fa-external-link-alt text-base"></i></span>
-                        </a>.
-                    </p>
-                </div>
-            </div>
-            <div className="py-4 ">
-                <RegistroComponent></RegistroComponent>
-            </div>
-
-        </section>
-    );
+export function CaratulaComponent() {
+  return (
+    <section className="flex flex-row flex-wrap justify-evenly py-10 px-3 caratula-bg">
+      <div className="grid content-evenly text-gray-700">
+        <h1 className="text-6xl font-extrabold">Educa Online</h1>
+        <h2 className="text-3xl md:text-4xl font-bold">Aprende con nosotros</h2>
+        <div>
+          <p className="text-2xl ">
+            Explora nuestros&nbsp;
+            <Link to="/cursos" className="underline text-indigo-700">
+              cursos
+            </Link>
+          </p>
+          <p className="text-2xl ">
+            Y accede a ellos en nuestra&nbsp;
+            <a href="/" className="underline text-indigo-700">
+              aula&nbsp;
+              <span>
+                <i className="fas fa-external-link-alt text-base"></i>
+              </span>
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+      <div className="py-4 ">
+        <RegistroComponent></RegistroComponent>
+      </div>
+    </section>
+  );
 }
